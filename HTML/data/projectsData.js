139dcef72733773f31cdacf3ea0acea1cfc309bb
madhan14 <madhankumar05072000@protonmail.com>
2025-12-18 17:47:51 +0530

    :zap: Update projects details

---
+++ HTML/data/projectsData.js
@@ -0,0 +1,25 @@
+const projectsData = [
+    {   id: 1,
+        imageUrl: 'img/project/1.png',
+        thumbnailUrl: 'img/project/1.png',
+        title: 'Silver Collections',
+        framework: 'Wordpress',
+        description: ` Silver Collections
+
+            Visit Silver Collections  the most gorgeous collection of  Silver Jewelry and Bronze Statues.
+
+            Located in the most prominent destination, Srirangam , which is acclaimed as the First among the 108 Divyadesams, the Silver Collections presents you an exotic variety of artifacts.
+
+            Srirangam, which houses the world’s largest functioning temple complex has been attracting tourists from India and abroad round the year. The temple reflects the rich tradition and culture of Tamil Nadu and people following Sri Vaishnavism  sect make it a point to visit Srirangam and worship the presiding deity Renganatha Swamy at least once in their lifetime.
+
+            With a standardized  factory set up in Swamimalai, which is quite popular for handmade bronze statues,  Silver Collections brings you the traditionally made statues of Gods and Goddesses. The unique features and the unparalleled finishing speaks volumes about the centuries old techniques of making handmade statues.
+
+            Some of the most alluring products available in Silver Collections include variety of jewelry items made of the purest form of Silver, Wooden panels with divine symbols and bronze statues.
+
+            When you visit Srirangam next time, include visiting Silver Collections in your wish list, which will be a compelling reason in your subsequent trips to this popular city.
+        `,
+        link: 'project_details.html?id=1',
+        siteLink: 'https://silvercollections.in/',
+        categoryName: 'projects'
+    }
+];
